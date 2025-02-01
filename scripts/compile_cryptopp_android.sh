27c9de8d1ea355f500241cc2b04456927c0bd919
Ramez Rafla <ramez@classroomapp.com>
2025-02-01 18:27:00 -0500

    Finally building on Android!!!

--- scripts/compile_cryptopp_android.sh
+++ scripts/compile_cryptopp_android.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ANDROID_PLATFORM=$1
-ANDROID_API="${ANDROID_PLATFORM: -2}" # from android-21 to 21
-CWD=$2 #Path to the root of the project
+export ANDROID_API="${ANDROID_PLATFORM: -2}" # getting android version -- i.e. android-21 --> 21
+CWD=$2 # Path to the root of the project
 export ANDROID_SDK_ROOT=$3
 export ANDROID_NDK_ROOT=$4
-TEMP_AND="TEMP_AND"
+TEMP_AND="$CWD/TEMP_ANDROID"
 
 # Do not compile if the library already exists
-if [[ -e "$CWD/cpp/android/libcryptopp_arm64-v8a.a" && 
-     -e "$CWD/cpp/android/libcryptopp_armeabi-v7a.a" && 
+if [[ -e "$CWD/cpp/android/libcryptopp_arm64-v8a.a" &&
+     -e "$CWD/cpp/android/libcryptopp_armeabi-v7a.a" &&
      -e "$CWD/cpp/android/libcryptopp_x86_64.a" &&
      -e "$CWD/cpp/android/libcryptopp_x86.a" ]]
 then
@@ -16,7 +16,7 @@ then
 fi
 
 echo "Root directory for react-native-cryptopp: $CWD"
-echo "Using API:${ANDROID_API}"
+echo "Using API: ${ANDROID_API}"
 echo "Android SDK: $ANDROID_SDK_ROOT"
 echo "Android NDK: $ANDROID_NDK_ROOT"
 echo -e "Crypto++ Compilation output: \n\n\n"
@@ -26,37 +26,57 @@ function build_cryptopp_android
     ANDROID_API="$1" ANDROID_CPU="$2" source TestScripts/setenv-android.sh
     make -f GNUmakefile-cross static
     make install -f GNUmakefile-cross PREFIX="$TEMP_AND"
-    mv "$TEMP_AND/lib/libcryptopp.a" "../cpp/android/libcryptopp_$2.a"
-    mv -f "$TEMP_AND/include/cryptopp" "../cpp"
+    mv "$TEMP_AND/lib/libcryptopp.a" "$CWD/cpp/android/libcryptopp_$2.a"
+    mv -f "$TEMP_AND/include/cryptopp" "$CWD/cpp"
 }
 
 # # #########################################
 # # #####           Prepare             #####
 # # #########################################
-pushd "$CWD"
-mkdir -p "cpp/cryptopp" "cpp/android"
+
+cd "$CWD"
+mkdir -p "$CWD/cpp/cryptopp"
+mkdir -p "$CWD/cpp/android"
 sh scripts/copy_pem_pack.sh
 
 # # #########################################
 # # #####            Android            #####
 # # #########################################
 
-pushd "cryptopp"
+cd "$CWD/cryptopp"
 make clean
 
 # Compile for all architectures
 build_cryptopp_android ${ANDROID_API} armeabi-v7a
+echo ""
+echo "Sleeping 5s ..."
+echo ""
+echo ""
+sleep 5
 make clean
 
 build_cryptopp_android ${ANDROID_API} arm64-v8a
+echo ""
+echo "Sleeping 5s ..."
+echo ""
+echo ""
+sleep 5
 make clean
 
 build_cryptopp_android ${ANDROID_API} x86
+echo ""
+echo "Sleeping 5s ..."
+echo ""
+echo ""
+sleep 5
 make clean
 
 build_cryptopp_android ${ANDROID_API} x86_64
+echo ""
+echo "Sleeping 5s ..."
+echo ""
+echo ""
+sleep 5
 make clean
 
 rm -rf "$TEMP_AND"
-popd
-popd
