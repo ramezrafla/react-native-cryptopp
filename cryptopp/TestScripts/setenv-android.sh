c51fd561e2cf4afc5603a1391a51b7a1de8e9a4a
Ramez Rafla <ramez@classroomapp.com>
2025-02-01 22:15:32 -0500

    removed intel from setenv-android.sh

--- cryptopp/TestScripts/setenv-android.sh
+++ cryptopp/TestScripts/setenv-android.sh
@@ -244,12 +244,12 @@ case "$ANDROID_CPU" in
     # ANDROID_CPPFLAGS="-D__ANDROID__=${ANDROID_API}"
 
     ANDROID_CFLAGS="-target i686-none-linux-android${ANDROID_API}"
-    ANDROID_CFLAGS="${ANDROID_CFLAGS} -march=i686 -mtune=intel -mssse3 -mfpmath=sse"
+    ANDROID_CFLAGS="${ANDROID_CFLAGS} -march=i686 -mssse3 -mfpmath=sse"
     ANDROID_CFLAGS="${ANDROID_CFLAGS} -fstack-protector-strong -funwind-tables -fexceptions -frtti"
     ANDROID_CFLAGS="${ANDROID_CFLAGS} -fno-addrsig -fno-experimental-isel"
 
     ANDROID_CXXFLAGS="-target i686-none-linux-android${ANDROID_API}"
-    ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -march=i686 -mtune=intel -mssse3 -mfpmath=sse"
+    ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -march=i686 -mssse3 -mfpmath=sse"
     ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -std=c++11 -stdlib=libc++"
     ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -fstack-protector-strong -funwind-tables -fexceptions -frtti"
     ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -fno-addrsig -fno-experimental-isel"
@@ -263,12 +263,12 @@ case "$ANDROID_CPU" in
     # ANDROID_CPPFLAGS="-D__ANDROID__=${ANDROID_API}"
 
     ANDROID_CFLAGS="-target x86_64-none-linux-android${ANDROID_API}"
-    ANDROID_CFLAGS="${ANDROID_CFLAGS} -march=x86-64 -msse4.2 -mpopcnt -mtune=intel"
+    ANDROID_CFLAGS="${ANDROID_CFLAGS} -march=x86-64 -msse4.2 -mpopcnt"
     ANDROID_CFLAGS="${ANDROID_CFLAGS} -fstack-protector-strong -funwind-tables -fexceptions -frtti"
     ANDROID_CFLAGS="${ANDROID_CFLAGS} -fno-addrsig -fno-experimental-isel"
 
     ANDROID_CXXFLAGS="-target x86_64-none-linux-android${ANDROID_API}"
-    ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -march=x86-64 -msse4.2 -mpopcnt -mtune=intel"
+    ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -march=x86-64 -msse4.2 -mpopcnt"
     ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -std=c++11 -stdlib=libc++"
     ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -fstack-protector-strong -funwind-tables -fexceptions -frtti"
     ANDROID_CXXFLAGS="${ANDROID_CXXFLAGS} -fno-addrsig -fno-experimental-isel"
