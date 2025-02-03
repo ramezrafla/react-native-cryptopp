e28ae1440d59799a0729784dbaa9f61402c357e7
Ramez Rafla <ramez@classroomapp.com>
2025-02-02 19:08:38 -0500

    final cleanups -- removed CI

--- cpp/hash-functions.cpp
+++ cpp/hash-functions.cpp
@@ -1,43 +1,42 @@
 #include "hash-functions.h"
 
 namespace rncryptopp::hash {
+  void hash(jsi::Runtime &rt, CppArgs *args, std::string *result) {
+    if (args->size() != 3)
+      throw facebook::jsi::JSError(rt, "RNCryptopp: hash invalid number of arguments");
 
-void hash(jsi::Runtime &rt, CppArgs *args, std::string *result) {
-  if (args->size() != 3)
-    throw facebook::jsi::JSError(rt, "RNCryptopp: hash invalid number of arguments");
+    QuickValue data = args->at(2);
 
-  QuickValue data = args->at(2);
+    if (!isDataStringOrAB(data))
+      throw facebook::jsi::JSError(rt, "RNCryptopp: hash data is not a string or ArrayBuffer");
 
-  if (!isDataStringOrAB(data))
-    throw facebook::jsi::JSError(rt, "RNCryptopp: hash data is not a string or ArrayBuffer");
-
-  std::string hash_type = args->at(1).stringValue;
-  if (hash_type == "SipHash_2_4_64") {
-    SipHash<2, 4, false> hash;
-    StringSource(data.stringValue, true, new HashFilter(hash, new HexEncoder(new StringSink(*result))));
-    return;
+    std::string hash_type = args->at(1).stringValue;
+    if (hash_type == "SipHash_2_4_64") {
+      SipHash<2, 4, false> hash;
+      StringSource(data.stringValue, true, new HashFilter(hash, new HexEncoder(new StringSink(*result))));
+      return;
+    }
+    else if (hash_type == "SipHash_4_8_128") {
+      SipHash<4, 8, true> hash;
+      StringSource(data.stringValue, true, new HashFilter(hash, new HexEncoder(new StringSink(*result))));
+      return;
+    }
+    else if (hash_type == "CRC32") {
+      CRC32 hash;
+      word32 digest = 0;
+      hash.CalculateDigest(
+          reinterpret_cast<CryptoPP::byte *>(&digest),
+          reinterpret_cast<CryptoPP::byte const *>(data.stringValue.data()),
+          data.stringValue.size());
+      std::stringstream ss;
+      ss << std::hex << digest;
+      *result = ss.str();
+      return;
+    }
+    else {
+      auto hasResult = invokeWithHash<rncryptopp::hash::calculate_hash>()(hash_type, &data.stringValue, result);
+      if (!hasResult)
+        throw facebook::jsi::JSError(rt, "RNCryptopp: hash - invalid hash name.");
+    }
   }
-  else if (hash_type == "SipHash_4_8_128") {
-    SipHash<4, 8, true> hash;
-    StringSource(data.stringValue, true, new HashFilter(hash, new HexEncoder(new StringSink(*result))));
-    return;
-  }
-  else if (hash_type == "CRC32") {
-    CRC32 hash;
-    word32 digest = 0;
-    hash.CalculateDigest(
-        reinterpret_cast<CryptoPP::byte *>(&digest),
-        reinterpret_cast<CryptoPP::byte const *>(data.stringValue.data()),
-        data.stringValue.size());
-    std::stringstream ss;
-    ss << std::hex << digest;
-    *result = ss.str();
-    return;
-  }
-  else {
-    auto hasResult = invokeWithHash<rncryptopp::hash::calculate_hash>()(hash_type, &data.stringValue, result);
-    if (!hasResult)
-      throw facebook::jsi::JSError(rt, "RNCryptopp: hash - invalid hash name.");
-  }
-}
 } // namespace rncryptopp::hash
