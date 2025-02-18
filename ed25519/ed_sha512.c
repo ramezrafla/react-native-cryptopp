f1d5880cc2e70d472792a17009d5461d200f5d00
Ramez Rafla <ramez@classroomapp.com>
2025-02-18 05:09:11 -0500

    removed std:: from ed_sha512.c

--- ed25519/ed_sha512.c
+++ ed25519/ed_sha512.c
@@ -10,7 +10,7 @@
  */
 
 /* the K array */
-static const std::uint64_t K[80] = {
+static const uint64_t K[80] = {
     0x428a2f98d728ae22, 0x7137449123ef65cd, 
     0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
     0x3956c25bf348b538, 0x59f111f1b605d019, 
@@ -56,38 +56,38 @@ static const std::uint64_t K[80] = {
 /* Various logical functions */
 
 #define ROR64c(x, y) \
-    ( ((((x)& 0xFFFFFFFFFFFFFFFF)>>((std::uint64_t)(y)& 63)) | \
-      ((x)<<((std::uint64_t)(64-((y)& 63))))) & 0xFFFFFFFFFFFFFFFF)
+    ( (((x& 0xFFFFFFFFFFFFFFFF)>>((uint64_t)y& 63)) | \
+      (x<<((uint64_t)(64-(y& 63))))) & 0xFFFFFFFFFFFFFFFF)
 
 #define STORE64H(x, y)                                                                     \
-   { (y)[0] = (unsigned char)(((x)>>56)&255); (y)[1] = (unsigned char)(((x)>>48)&255);     \
-     (y)[2] = (unsigned char)(((x)>>40)&255); (y)[3] = (unsigned char)(((x)>>32)&255);     \
-     (y)[4] = (unsigned char)(((x)>>24)&255); (y)[5] = (unsigned char)(((x)>>16)&255);     \
-     (y)[6] = (unsigned char)(((x)>>8)&255); (y)[7] = (unsigned char)((x)&255); }
+   { y[0] = (unsigned char)((x>>56)&255); y[1] = (unsigned char)((x>>48)&255);     \
+     y[2] = (unsigned char)((x>>40)&255); y[3] = (unsigned char)((x>>32)&255);     \
+     y[4] = (unsigned char)((x>>24)&255); y[5] = (unsigned char)((x>>16)&255);     \
+     y[6] = (unsigned char)((x>>8)&255); y[7] = (unsigned char)(x&255); }
 
 #define LOAD64H(x, y)                                                      \
-   { x = (((std::uint64_t)((y)[0] & 255))<<56)|(((std::uint64_t)((y)[1] & 255))<<48) | \
-         (((std::uint64_t)((y)[2] & 255))<<40)|(((std::uint64_t)((y)[3] & 255))<<32) | \
-         (((std::uint64_t)((y)[4] & 255))<<24)|(((std::uint64_t)((y)[5] & 255))<<16) | \
-         (((std::uint64_t)((y)[6] & 255))<<8)|(((std::uint64_t)((y)[7] & 255))); }
+   { x = (((uint64_t)(y[0] & 255))<<56)|(((uint64_t)(y[1] & 255))<<48) | \
+         (((uint64_t)(y[2] & 255))<<40)|(((uint64_t)(y[3] & 255))<<32) | \
+         (((uint64_t)(y[4] & 255))<<24)|(((uint64_t)(y[5] & 255))<<16) | \
+         (((uint64_t)(y[6] & 255))<<8)|(((uint64_t)(y[7] & 255))); }
 
 
 #define Ch(x,y,z)       (z ^ (x & (y ^ z)))
 #define Maj(x,y,z)      (((x | y) & z) | (x & y)) 
 #define S(x, n)         ROR64c(x, n)
-#define R(x, n)         (((x) & 0xFFFFFFFFFFFFFFFF)>>((std::uint64_t)n))
-#define Sigma0(x)       (S(x, 28) ^ S(x, 34) ^ S(x, 39))
-#define Sigma1(x)       (S(x, 14) ^ S(x, 18) ^ S(x, 41))
-#define Gamma0(x)       (S(x, 1) ^ S(x, 8) ^ R(x, 7))
-#define Gamma1(x)       (S(x, 19) ^ S(x, 61) ^ R(x, 6))
+#define R(x, n)         ((x & 0xFFFFFFFFFFFFFFFF)>>((uint64_t)n))
+#define Sigma0x       (S(x, 28) ^ S(x, 34) ^ S(x, 39))
+#define Sigma1x       (S(x, 14) ^ S(x, 18) ^ S(x, 41))
+#define Gamma0x       (S(x, 1) ^ S(x, 8) ^ R(x, 7))
+#define Gamma1x       (S(x, 19) ^ S(x, 61) ^ R(x, 6))
 #ifndef MIN
-   #define MIN(x, y) ( ((x)<(y))?(x):(y) )
+   #define MIN(x, y) ( (x<y)?x:y )
 #endif
 
 /* compress 1024-bits */
 static int sha512_compress(sha512_context *md, unsigned char *buf)
 {
-    std::uint64_t S[8], W[80], t0, t1;
+    uint64_t S[8], W[80], t0, t1;
     int i;
 
     /* copy state into S */
